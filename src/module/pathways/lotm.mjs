ae2c4f59778f69b8812aea138d8d043c046fef5d
Enzo Lopez <aleksandr.btncrt@pm.me>
2025-11-18 00:24:04 -0300

    add missing pathways of lotm

--- src/module/pathways/lotm.mjs
+++ src/module/pathways/lotm.mjs
@@ -1,30 +1,20 @@
-export const foolPathway = {
-  '0': { name: 'LOTM.Pathways.fool.0', sequence: "0" },
-  '1': { name: 'LOTM.Pathways.fool.1', sequence: "1" },
-  '2': { name: 'LOTM.Pathways.fool.2', sequence: "2" },
-  '3': { name: 'LOTM.Pathways.fool.3', sequence: "3" },
-  '4': { name: 'LOTM.Pathways.fool.4', sequence: "4" },
-  '5': { name: 'LOTM.Pathways.fool.5', sequence: "5" },
-  '6': { name: 'LOTM.Pathways.fool.6', sequence: "6" },
-  '7': { name: 'LOTM.Pathways.fool.7', sequence: "7" },
-  '8': { name: 'LOTM.Pathways.fool.8', sequence: "8" },
-  '9': { name: "LOTM.Pathways.fool.9", sequence: "9" }
-}
+import generatePathwayTree from "../utils/generate-pathway-tree.mjs"
+
 
 export const lordOfMysteries = {
   fool: {
     name: 'Fool Pathway',
     key: 'fool',
-    tree: foolPathway
+    tree: generatePathwayTree("fool")
   },
   error: {
     name: 'Error Pathway',
     key: 'error',
-    tree: []
+    tree: generatePathwayTree("error")
   },
   door: {
     name: 'Door Pathway',
-    key: "error",
-    tree: []
+    key: "door",
+    tree: generatePathwayTree("door")
   }
 }
---
+++ src/module/utils/generate-pathway-tree.mjs
@@ -0,0 +1,48 @@
+/**
+ * 
+ * @param {string} pathway 
+ */
+export default function (pathway) {
+  return {
+    '0': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.0`,
+      sequence: "0"
+    },
+    '1': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.1`,
+      sequence: "1"
+    },
+    '2': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.2`,
+      sequence: "2"
+    },
+    '3': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.3`,
+      sequence: "3"
+    },
+    '4': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.4`,
+      sequence: "4"
+    },
+    '5': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.5`,
+      sequence: "5"
+    },
+    '6': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.6`,
+      sequence: "6"
+    },
+    '7': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.7`,
+      sequence: "7"
+    },
+    '8': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.8`,
+      sequence: "8"
+    },
+    '9': {
+      name: `LOTM.Pathways.${pathway.toLowerCase()}.9`,
+      sequence: "9"
+    }
+  }
+}
